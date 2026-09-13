7cd2a92d49203135712aded1e233d725e3fc771b
danwhite-lab <danielcwhite@gmail.com>
2026-09-13 16:13:56 +0300

    Bundle Supabase client for production

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='on-my-mind-shell-v5';
+const CACHE='on-my-mind-shell-v6';
 const ASSETS=['./','./index.html','./supabase-client.js','./manifest.webmanifest','./apple-touch-icon.png','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key.startsWith('on-my-mind-shell-')&&key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()));});
